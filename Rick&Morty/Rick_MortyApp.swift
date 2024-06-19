ccabc2805b8c24ae650f804a85959ee8d3711e6c
Mateusz <matikg@interia.eu>
2024-06-19 10:39:02 +0200

    Initial Commit

---
+++ Rick&Morty/Rick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyApp.swift
+//  Rick&Morty
+//
+//  Created by Mateusz Grudzień on 19/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
